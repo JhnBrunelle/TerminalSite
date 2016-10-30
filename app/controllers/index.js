ee28698abc83d7fc9779fe1bda4717659931f823
John Brunelle <Johnzors@gmail.com>
2016-10-30 06:06:40 -0400

    Began adding commands, changed page title

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,8 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  numberOfLines: 4,
-  textRows: 18,
+  numberOfLines: 4,   // The amount of lines currently displayed on screen
+  textRows: 18,       // Number of Rows the Terminal Window can hold
   commandHistory:[
     {message :  "Hello, My name is Johnathan Brunelle."},
     {message :  "I am an Electrical Computer Engineering Student in my second"},
@@ -11,18 +11,19 @@ export default Ember.Controller.extend({
     {message :  "Enter 'help' to view the commands."}],
 
   init: function() {
+
     this.set('textBuffer', '> ');
   },
 
-  /*
-   * Function to maintain correct number of lines
-   * Always will keep this
+  /**
+   * Ensures that text does not run off the terminal window
+   * @param historyList current text displayed
    */
   checkLines: function(historyList){
 
     if(this.get('numberOfLines') > this.get('textRows')){
 
-      // Shift a certain amount
+      // Shift the queue a certain amount
       let difference = this.get('numberOfLines') - this.get('textRows');
 
       for(var i = 0; i < difference; i++){
@@ -33,26 +34,52 @@ export default Ember.Controller.extend({
     }
   },
 
-  // Match the Command to a function and perform
+  /**
+   * Process the user input, pointing it to correct function
+   * and providing a response
+   * @param cmnd user command
+   */
   registerCommand : function(cmnd){
     var historyList = this.get("commandHistory");
-    this.set("numberOfLines", this.get("numberOfLines") + 2);
-    this.checkLines(historyList);
-    historyList.pushObject({message:cmnd});
-    historyList.pushObject({message:"jsh: " + cmnd + ": command not found"});
+
 
     // Used for debugging
-    if(cmnd === "> stabilitycheck"){
-      console.log("Command History: " + this.get("commandHistory").length);
-      console.log("Number Of Lines: " + this.get("numberOfLines"));
+    switch(cmnd) {
+      case "> clear":
+            this.set('commandHistory', []);
+            this.set('numberOfLines', 0);
+            break;
+
+      case "> stabilitycheck":    // Used for Debugging
+        console.log("commandHistory: " + this.get('commandHistory').length);
+        console.log("numberOfLines:  " + this.get('numberOfLines'));
+
+        this.set("numberOfLines", this.get("numberOfLines") + 2);
+        this.checkLines(historyList);
+        historyList.pushObject({message:cmnd});
+        historyList.pushObject({message:"Stability Checked! View Console!"});
+        break;
+
+      default:
+        this.set("numberOfLines", this.get("numberOfLines") + 2);
+        this.checkLines(historyList);
+        historyList.pushObject({message:cmnd});
+        historyList.pushObject({message:"jsh: " + cmnd + ": command not found"});
+        break;
+
     }
+
+
 
   },
 
   actions: {
 
+    /**
+     * Handlees input from console command line
+     * @param text User Input
+     */
     inputLineHandler : function(text){
-      var historyList = this.get("commandHistory");
       this.registerCommand(text);
       this.set('textBuffer', '> ');
     }
