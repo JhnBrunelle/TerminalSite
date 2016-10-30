2a2dd84600e4369fec73adb32f7cdf0449e97b84
John Brunelle <Johnzors@gmail.com>
2016-10-30 03:08:42 -0400

    Got basic terminal working

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,30 +2,39 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  numberOfLines: 15,
+  numberOfLines: 4,
   commandHistory:[
-    {message : "Hello"},
-    {message :  "This"},
-    {message :  "Type"}],
+    {message : "Hello, My name is Johnathan Brunelle, and welcome to my site."},
+    {message :  "I am currently studying Electrical Computer Engineering at the"},
+    {message :  "at the university of Western Ontario. Enter command 'help' to"},
+    {message :  "view commands"}],
 
+  init: function() {
+
+  },
+
+  // Only maintains 15 history items at a time
   shiftList: function(historyList){
+    console.log(historyList.length);
     if(historyList.length === 15){
       this.get('commandHistory').set('commandHistory',historyList.shift());
     }
   },
 
+  // Send text to command
+  registerCommand : function(cmnd){
+    var historyList = this.get("commandHistory");
+    this.shiftList(historyList);
+    historyList.pushObject({message:"-sh: " + cmnd + ": command not found"});
+  },
+
   actions: {
-    addItem: function() {
-      console.log(this.get("commandHistory"));
-      var historyList = this.get("commandHistory");
-      historyList.pushObject({message:"hello"});
-    },
 
     inputLineHandler : function(text){
       console.log(text);
       var historyList = this.get("commandHistory");
       this.shiftList(historyList);
-      historyList.pushObject({message:text});
+      this.registerCommand(text);
       this.set('textBuffer', '');
     }
   }
