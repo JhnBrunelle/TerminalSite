5293d5d10bc2229f01eef64a24f22954d383fc6a
John Brunelle <Johnzors@gmail.com>
2017-02-19 03:38:13 -0500

    input validation, made it look more like a real terminal, changed image, fixed caret issue

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -6,13 +6,13 @@ export default Ember.Controller.extend({
   textRows: 18,       // Number of Rows the Terminal Window can hold
   commandHistory: [
     {message: "Hello, My name is Johnathan Brunelle."},
-    {message: "I am an Electrical Engineering Student in my second"},
+    {message: "I am an Electrical Engineering + CS Student in my second"},
     {message: "year at the University of Western Ontario."},
     {message: "Enter 'help' to view the commands."}],
 
   init: function () {
 
-    this.set('textBuffer', '> ');
+    this.set('textBuffer', '');
   },
 
   /**
@@ -52,8 +52,8 @@ export default Ember.Controller.extend({
 
     // Used for debugging
     switch (cmnd) {
-      case "> help":
-      case "> ls":
+      case "help":
+      case "ls":
         this.set("numberOfLines", this.get("numberOfLines") + 8);
         this.checkLines(historyList);
         historyList.pushObject({message: cmnd});
@@ -66,12 +66,12 @@ export default Ember.Controller.extend({
         historyList.pushObject({message: "  contact - Display Contact Info"});
         break;
 
-      case "> clear":
+      case "clear":
         this.set('commandHistory', []);
         this.set('numberOfLines', 0);
         break;
 
-      case "> stabilitycheck":    // Used for Debugging
+      case "stabilitycheck":    // Used for Debugging
         console.log("commandHistory: " + this.get('commandHistory').length);
         console.log("numberOfLines:  " + this.get('numberOfLines'));
 
@@ -81,7 +81,7 @@ export default Ember.Controller.extend({
         historyList.pushObject({message: "Stability Checked! View Console!"});
         break;
 
-      case "> contact":    // Used for Debugging
+      case "contact":    // Used for Debugging
         console.log("commandHistory: " + this.get('commandHistory').length);
         console.log("numberOfLines:  " + this.get('numberOfLines'));
 
@@ -91,7 +91,7 @@ export default Ember.Controller.extend({
         historyList.pushObject({message: "Email - jhnbrunelle@gmail.com"});
         break;
 
-      case "> github":
+      case "github":
         this.set("numberOfLines", this.get("numberOfLines") + 2);
         this.checkLines(historyList);
         historyList.pushObject({message: cmnd});
@@ -99,7 +99,7 @@ export default Ember.Controller.extend({
         historyList.pushObject({message: "Github opened!"});
         break;
 
-      case "> linkedin":
+      case "linkedin":
         this.set("numberOfLines", this.get("numberOfLines") + 2);
         this.checkLines(historyList);
         historyList.pushObject({message: cmnd});
@@ -107,7 +107,7 @@ export default Ember.Controller.extend({
         historyList.pushObject({message: "Linkedin opened!"});
         break;
 
-      case "> degree":    // States my Degree
+      case "degree":    // States my Degree
         this.set("numberOfLines", this.get("numberOfLines") + 4);
         this.checkLines(historyList);
         historyList.pushObject({message: cmnd});
@@ -116,7 +116,7 @@ export default Ember.Controller.extend({
         historyList.pushObject({message: "GPA: 3.7, Class of 2019"});
         break;
 
-      case "> about":
+      case "about":
         this.set("numberOfLines", this.get("numberOfLines") + 6);
         this.checkLines(historyList);
         historyList.pushObject({message: cmnd});
@@ -130,13 +130,24 @@ export default Ember.Controller.extend({
       default:
         this.set("numberOfLines", this.get("numberOfLines") + 2);
         this.checkLines(historyList);
-        historyList.pushObject({message: cmnd});
+        if(cmnd === ''){
+          historyList.pushObject({message: " "});
+        }else{
+          historyList.pushObject({message: cmnd});
+        }
         historyList.pushObject({message: "jsh: " + cmnd + ": command not found"});
         break;
-
     }
 
 
+  },
+
+  // Used to validate correct console input
+  // Returns true if valid, or false if invalid
+  // Invalid - Nothing but spaces ie. Null input
+  validateInput(input){
+    let parsedInput = input.replace(/\s+/, "");
+    return parsedInput !== "";
   },
 
   actions: {
@@ -146,8 +157,10 @@ export default Ember.Controller.extend({
      * @param text User Input
      */
     inputLineHandler: function (text) {
-      this.registerCommand(text);
-      this.set('textBuffer', '> ');
+      if(this.validateInput(text)){
+        this.registerCommand(text);
+        this.set('textBuffer', '');
+      }
     }
   }
 });
